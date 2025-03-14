58c9daae764f7101256d49417ad0a9349600ca21
Akelpha <kerennzira@gmail.com>
2025-03-14 01:40:22 +0100

    Add PostCSS configuration and global styles; update layout metadata

--- .next/static/chunks/src_app_layout_tsx_f0e4c1a2._.js
+++ .next/static/chunks/src_app_layout_tsx_f0e4c1a2._.js
@@ -5,6 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_layout_tsx_f0e4c1a2._.js",
   "chunks": [
+    "static/chunks/src_styles_global_99ee5e55.css",
     "static/chunks/node_modules_next_dist_1a6ee436._.js"
   ],
   "source": "dynamic"
