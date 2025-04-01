941ebab406052c9ffa1ac0b55a4182a9bf3f5419
Akelpha <kerennzira@gmail.com>
2025-04-01 15:03:58 +0100

    Add eslint-config-prettier to package.json and update CommandCart component styling

--- src/app/components/commandCart.tsx
+++ src/app/components/commandCart.tsx
@@ -3,13 +3,15 @@ import React from "react";
 export default function CommandCart() {
   return (
     <div className="bg-white flex flex-col justify-center items-center gap-4 w-[90%] h-[300px] mx-auto rounded-xl mt-10 shadow-lg">
-      <h2>
+      <h2 className="text-lg font-bold text-red">
         Your Cart <span>(0)</span>
       </h2>
       <img
         src="/assets/images/illustration-empty-cart.svg"
         alt="Illustration Icon"></img>
-      <p>Your added items will appear here.</p>
+      <p className="text-rose-900 font-bold">
+        Your added items will appear here.
+      </p>
     </div>
   );
 }
